124fdff6bb2a0a0d52ebcccb09be55720cf3de94
sebarina.xl <sebarina.xl@alibaba-inc.com>
2016-05-23 17:29:29 +0800

    keychain access lib

---
+++ UTKeychainSecurity/UTKeychainSecurity.h
@@ -0,0 +1,19 @@
+//
+//  UTKeychainSecurity.h
+//  UTKeychainSecurity
+//
+//  Created by Sebarina Xu on 5/23/16.
+//  Copyright © 2016 liufan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UTKeychainSecurity.
+FOUNDATION_EXPORT double UTKeychainSecurityVersionNumber;
+
+//! Project version string for UTKeychainSecurity.
+FOUNDATION_EXPORT const unsigned char UTKeychainSecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UTKeychainSecurity/PublicHeader.h>
+
+
